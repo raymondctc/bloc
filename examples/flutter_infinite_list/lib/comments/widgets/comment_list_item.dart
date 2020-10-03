2a04212a7ff53d79ebe84709f061c42ef3ba7efe
Raymond Chan <raymond@9gag.com>
2020-10-03 22:50:40 +0800

    Support video

--- examples/flutter_infinite_list/lib/comments/widgets/comment_list_item.dart
+++ examples/flutter_infinite_list/lib/comments/widgets/comment_list_item.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_infinite_list/comments/comments.dart';
+import 'package:video_player/video_player.dart';
 
-class CommentListItem extends StatelessWidget {
+class CommentListItem extends StatefulWidget {
   const CommentListItem({
     Key key,
     @required this.comment,
@@ -12,7 +13,35 @@ class CommentListItem extends StatelessWidget {
   final User user;
 
   @override
+  _CommentListVideoItemState createState() => _CommentListVideoItemState();
+}
+
+class _CommentListVideoItemState extends State<CommentListItem> {
+  VideoPlayerController videoPlayerController ;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.comment.media != null && widget.comment.media.first.imageMetaByType.video != null) {
+      videoPlayerController = VideoPlayerController.network(widget.comment.media.first.imageMetaByType.video.url);
+      _initializeVideoPlayerFuture = videoPlayerController.initialize()
+          .then((value) => { setState(() {}) });
+    }
+  }
+
+  @override
+  void dispose() {
+    if (videoPlayerController != null) {
+      videoPlayerController.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final comment = widget.comment;
+    final user = widget.user;
     final textTheme = Theme.of(context).textTheme;
     final boxWidth = MediaQuery.of(context).size.width * 0.7;
 
@@ -28,7 +57,80 @@ class CommentListItem extends StatelessWidget {
       switch (type) {
         case 'ANIMATED':
           if (firstMedia.imageMetaByType.video != null) {
-
+            return FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Container(
+                    margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Container(
+                          child: CircleAvatar(
+                              backgroundImage: NetworkImage(user.avatarUrl)
+                          ),
+                          padding: const EdgeInsets.fromLTRB(16, 0, 8, 0),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: const BorderRadius.all(Radius.circular(8))
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                  child: Text(
+                                    user.displayName,
+                                    style: const TextStyle(fontWeight: FontWeight.bold),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  padding: const EdgeInsets.fromLTRB(8, 4, 4, 0)
+                              ),
+                              ConstrainedBox(
+                                constraints: BoxConstraints(
+                                    maxWidth: boxWidth
+                                ),
+                                child: Container(
+                                  padding: const EdgeInsets.fromLTRB(8, 4, 4, 8),
+                                  child: Text(
+                                    comment.text,
+                                    textAlign: TextAlign.left,
+                                  ),
+                                ),
+                              ),
+                              GestureDetector(
+                                child: videoPlayerController.value.initialized
+                                ? Container(
+                                    width: boxWidth,
+                                    height: boundedHeight,
+                                    child: VideoPlayer(videoPlayerController)
+                                ) : Container(),
+                                onTap: () {
+                                  setState(() {
+                                    if (videoPlayerController.value.isPlaying) {
+                                      videoPlayerController.pause();
+                                    } else {
+                                      videoPlayerController.play();
+                                    }
+                                  });
+                                },
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                } else {
+                  return Container(
+                    width: boxWidth,
+                    height: boundedHeight
+                  );
+                }
+              },
+            );
           }
           break;
         case 'STATIC':
@@ -45,6 +147,9 @@ class CommentListItem extends StatelessWidget {
       }
     }
 
+
+
+
     var commentContainer = Container(
       margin: const EdgeInsets.fromLTRB(0, 8, 0, 8),
       child: Row(
@@ -52,7 +157,7 @@ class CommentListItem extends StatelessWidget {
         children: [
           Container(
             child: CircleAvatar(
-              backgroundImage: NetworkImage(user.avatarUrl)
+                backgroundImage: NetworkImage(user.avatarUrl)
             ),
             padding: const EdgeInsets.fromLTRB(16, 0, 8, 0),
           ),
@@ -74,7 +179,7 @@ class CommentListItem extends StatelessWidget {
                 ),
                 ConstrainedBox(
                   constraints: BoxConstraints(
-                    maxWidth: boxWidth
+                      maxWidth: boxWidth
                   ),
                   child: Container(
                     padding: const EdgeInsets.fromLTRB(8, 4, 4, 8),
